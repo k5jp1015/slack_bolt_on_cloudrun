944987f96c6ac1d66f9436d179c4be14bf11d759
K5Magami <keigo.magami@webcrew.co.jp>
2019-09-21 11:19:22 +0900

    webpack 4 + Babel 7 + TypeScript + TypeScript EsLint + Prettierの開発環境を構築 検証
    https://qiita.com/soarflat/items/d583356e46250a529ed5

---
+++ app/src/ts/app.ts
@@ -0,0 +1,3 @@
+import add from './modules/add';
+
+console.log(add(10, 5));
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,17 +1,46 @@
-// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
-const path = require('path');
+const { resolve } = require('path');
 
 module.exports = {
-  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
-  mode: 'development',
-  target: 'node',
   // エントリーポイントの設定
-  entry: './src/js/app.js',
+  entry: './src/ts/app.ts',
   // 出力の設定
   output: {
     // 出力するファイル名
-    filename: 'bundle.js',
+    filename: 'bundle_2.js',
     // 出力先のパス（絶対パスを指定する必要がある）
-    path: path.join(__dirname, 'public/js')
+    path: resolve(__dirname, 'public/js')
+  },
+  resolve: {
+    extensions: ['.ts']
+  },
+  module: {
+    rules: [
+      {
+        // ローダーの処理対象ファイル
+        test: /\.ts$/,
+        // 利用するローダー
+        use: 'babel-loader',
+        // ローダーの処理対象から外すディレクトリ
+        exclude: /node_modules/
+      }
+    ]
   }
 };
+
+// // output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+// const path = require('path');
+
+// module.exports = {
+//   // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+//   mode: 'development',
+//   target: 'node',
+//   // エントリーポイントの設定
+//   entry: './src/js/app.js',
+//   // 出力の設定
+//   output: {
+//     // 出力するファイル名
+//     filename: 'bundle.js',
+//     // 出力先のパス（絶対パスを指定する必要がある）
+//     path: path.join(__dirname, 'public/js')
+//   }
+// };
